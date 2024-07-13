59f7f9da920fd2e9d287074cfa6e473eacb28d4f
IllIlIIlIlllIIlIIllIllllIllIlllI <paul@rosegarden.rosegarden>
2024-07-13 11:15:52 +0100

    added resume

--- components/homepage/BlogLinks.tsx
+++ components/homepage/BlogLinks.tsx
@@ -1,18 +1,19 @@
 import Link from '@/components/Link';
 import Twemoji from '@/components/Twemoji';
+import Image from 'next/image';
 
 const BlogLinks = () => {
   return (
     <div className="flex justify-between ">
       <div className="flex flex-col space-y-1.5">
         <Link href="/blog" className="hover:underline">
-          <Twemoji emoji="memo" />
+          <Twemoji emoji="man-technologist" />
           <span data-umami-event="home-link-blog" className="ml-1.5">
             My writings
           </span>
         </Link>
         <Link href="/projects" className="hover:underline">
-          <Twemoji emoji="hammer-and-wrench" />
+          <Twemoji emoji="hourglass-not-done" />
           <span data-umami-event="home-link-projects" className="ml-1.5">
             What have I built?
           </span>
@@ -20,7 +21,7 @@ const BlogLinks = () => {
       </div>
       <div className="flex flex-col space-y-1.5">
         <Link href="/about" className="hover:underline">
-          <Twemoji emoji="face-with-monocle" />
+          <Twemoji emoji="european-union-flag" />
           <span data-umami-event="home-link-about" className="ml-1.5">
             More about me and myself
           </span>
